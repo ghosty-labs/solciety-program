50570a86648e8aaf3a77dbe84520149944ab1d09
Iqbal Utomo <iqbalutomo@Iqbals-MacBook-Pro.local>
2023-09-12 04:17:43 +0700

    feat: define and size post account

--- programs/solciety-program/src/lib.rs
+++ programs/solciety-program/src/lib.rs
@@ -13,3 +13,21 @@ pub mod solciety_program {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+pub struct Post {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
+impl Post {
+    const LEN: usize = DISCRIMINATOR_LENGTH + PUBLIC_KEY_LENGTH + TIMESTAMP_LENGTH + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH + MAX_CONTENT_LENGTH;
+}
